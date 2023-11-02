7b6e4ababdd2ad05ffeb8f52d44ba6b01acdda06
gungun005 <g053780@gmail.com>
2023-11-02 14:20:14 +0530

    services added

---
+++ services/flipService.js
@@ -0,0 +1,71 @@
+const flipRepository=require('../repository/flipRepository');
+
+const getFlipRegisters=(body)=>{
+console.log("in services");
+console.log(body);
+let getFlipRegisters=flipRepository.getFlipRegisters(body);
+return(getFlipRegisters);
+};
+const postFlipRegisters=(body)=>{
+console.log("in services");
+console.log(body);
+let postFlipRegisters=flipRepository.postFlipRegisters(body);
+return(postFlipRegisters);
+};
+    
+const getFlipLogin=(body)=>{
+console.log("in services");
+console.log(body);
+let  getFlipLogin=flipRepository.getFlipLogin(body);
+return( getFlipLogin);
+};
+const postFlipLogin=(body)=>{
+console.log("in services");
+console.log(body);
+let postFlipLogin=flipRepository.postFlipLogin(body);
+return(postFlipLogin);
+};
+
+const getFlipFpsswrdUser=(body)=>{
+console.log("in services");
+console.log(body);
+let getFlipFpsswrdUser=flipRepository.getFlipFpsswrdUser(body);
+return(getFlipFpsswrdUser);
+};
+const postFlipFpsswrdUser=(body)=>{
+console.log("in services");
+console.log(body);
+let  postFlipFpsswrdUser=flipRepository.postFlipFpsswrdUser(body);
+return( postFlipFpsswrdUser);
+};
+    
+const getFlipCpsswrdUser=(body)=>{
+console.log("in services");
+console.log(body);
+let getFlipCpsswrdUser=flipRepository.getFlipCpsswrdUser(body);
+return(getFlipCpsswrdUser);
+};
+const postFlipCpsswrdUser=(body)=>{
+console.log("in services");
+console.log(body);
+let postFlipCpsswrdUser=flipRepository.postFlipCpsswrdUser(body);
+return(postFlipCpsswrdUser);
+};
+        
+const delFlipUser=(body)=>{
+console.log("in services");
+console.log(body);
+let delFlipUser=flipRepository.delFlipUser(body);
+return(delFlipUser);
+};
+    module.exports={
+        getFlipRegisters,
+        postFlipRegisters,
+        getFlipLogin,
+        postFlipLogin,
+        getFlipFpsswrdUser,
+        postFlipFpsswrdUser,
+        getFlipCpsswrdUser,
+        postFlipCpsswrdUser,
+        delFlipUser
+    };
